feeb350543ae9c316b4b59139aab91617ae9e022
曲廉卿 <1483523635@qq.com>
2017-05-25 10:33:19 +0800

    CefSharp simple Demo

---
+++ CefSharpSimpleDemo/WebPage.cs
@@ -0,0 +1,25 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System.Windows.Forms;
+
+namespace CefSharpSimpleDemo
+{
+    public partial class WebPage : Form
+    {
+        public WebPage()
+        {
+            InitializeComponent();
+            GetWebPage();
+        }
+        public void GetWebPage()
+        {
+            CefSettings settings = new CefSettings();
+            Cef.Initialize(settings);
+            ChromiumWebBrowser webbrowser = new ChromiumWebBrowser("https://www.baidu.com/")
+            { Dock = DockStyle.Fill };
+            this.Controls.Add(webbrowser);
+            webbrowser.Dock = DockStyle.Fill;
+        }
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Simple CefSharp Demo, For Details to My Bolg : http://www.cnblogs.com/qulianqing/p/6902369.html
